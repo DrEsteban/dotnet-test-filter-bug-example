6e845dba9fd34175fab4190350de9465f78d84ba
Steven Ryland <steven.ryland@infinicept.com>
2022-09-22 16:48:53 -0700

    `dotnet test --filter` bug example

---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(
+            "1234567890", 
+            "1234567890", 
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "1234567890",
+            "123456",
+            "1234567890")]
+        public void ThisIsAnExtremelyLongTestName_SoICanDemonstrateAStrangeBehaviorInDotnetTest_Filter(
+            string string1,
+            string string2,
+            string string3,
+            string string4,
+            string string5,
+            string string6,
+            string string7,
+            string string8,
+            string string9,
+            string string10,
+            string string11,
+            string string12,
+            string string13,
+            string string14,
+            string string15)
+        {
+            //Assert.False(true);
+        }
+    }
+}
